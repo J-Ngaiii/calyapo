a1c6afdba8bef9b49568bf85f402cb5e7bc360c7
jngaiii <jngai_@berkeley.edu>
2025-10-09 23:20:52 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CalyAPO
+
+CalyAPO is Jonathan Ngai's Data Science Honors Thesis repository for analyzing California public opinion data and training neural net models.
---
+++ calyapo/training/configs/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
+from calyap.training.configs.peft import lora_config, llama_adapter_config, prefix_config
+from calyap.training.configs.fsdp import fsdp_config
+from calyap.training.configs.training import train_config
+from calyap.training.configs.wandb import wandb_config
+from calyap.training.configs.quantization import quantization_config
