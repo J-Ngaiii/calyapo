853bb37f47da12ff54765812a24d060d06b65a4c
J-Ngaiii <jngai_@berkeley.edu>
2026-05-12 00:19:24 -0700

    updated quick_wd.py with agrparse and creating a folder in the working directory

--- scripts/data_eval/quick_wd.py
+++ scripts/data_eval/quick_wd.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import argparse
+from pathlib import Path
+
 import matplotlib.pyplot as plt
 
-def generate_best_plot(df, split, score: str):
+def generate_best_plot(df, split, train_plan, score: str):
+    """
+    We average across survey questions in the train plan
+    Select best model performance by selecting the model that has min score
+    """
     df = df.copy()[df['Split'] == split]
     avg_scores = df.groupby(['Demographic', 'Model'])[score].mean().reset_index()
     best_models = avg_scores.loc[avg_scores.groupby('Demographic')[score].idxmin()]
+    best_models = best_models.sort_values(by=score, ascending=False)
 
     unique_models = best_models['Model'].unique()
     colors_palette = plt.cm.get_cmap('tab10', len(unique_models))
@@ -20,15 +28,16 @@ def generate_best_plot(df, split, score: str):
         plt.text(bar.get_x() + bar.get_width()/2, yval, f'{model_name}', 
                 va='bottom', ha='center', fontsize=6, fontweight='bold')
 
-    plt.title(f'Best Model per Demographic Group (Min Weighted {score} on Test Set)', fontsize=14)
+    plt.title(f'Best Model per Demographic Group ({train_plan})', fontsize=14)
     plt.xlabel('Demographic Group', fontsize=12)
-    plt.ylabel(f'Average {score}', fontsize=12)
+    plt.ylabel(f'Average {score} Score', fontsize=12)
     plt.xticks(rotation=45, ha='right')
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.tight_layout()
 
-
-    save_path = f"./{score}"
+    quick_dir_name = f"{train_plan}_quick_results"
+    Path(quick_dir_name).mkdir(parents=True, exist_ok=True)
+    save_path = f"{quick_dir_name}/{train_plan}_{score}"
     plt.savefig(save_path, dpi=300, bbox_inches='tight')
     print(f"Plot saved to: {save_path}")
 
@@ -36,6 +45,12 @@ def generate_best_plot(df, split, score: str):
     plt.show()
 
 if __name__ == "__main__":
-    cols = ['KL_Unweighted', 'WD_Unweighted', 'KL_Weighted', 'WD_Weighted']
-    df = pd.read_csv('inference_outputs/opinion_school/reports_aurora/results/distributional_accuracy/summary_demog_metrics.csv')
-    generate_best_plot(df=df, split='test', score='KL_Weighted')
+    parser = argparse.ArgumentParser(description="Runs analysis of offline inference data.") 
+    parser.add_argument("--train_plan", type=str, nargs='?', default='opinion_school', help="Name of training plan.")
+    parser.add_argument("--run_keyword", type=str, nargs='?', default='archon', help="Keyword corresponding with inference run.")
+    parser.add_argument("--score", type=str, choices=['KL_Unweighted', 'WD_Unweighted', 'KL_Weighted', 'WD_Weighted'], default='WD_Weighted')
+    
+    args = parser.parse_args()
+    
+    df = pd.read_csv(f'inference_outputs/{args.train_plan}/reports_{args.run_keyword}/results/distributional_accuracy/summary_demog_metrics.csv')
+    generate_best_plot(df=df, split='test', train_plan=args.train_plan, score=args.score)
